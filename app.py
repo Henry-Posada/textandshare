9e953adfc786a247ae733687fd87377d50bd8514
Henry Posada <henryposada@mail.adelphi.edu>
2021-03-19 16:36:17 -0400

    Sprint 3 3/19/2021 v2, Adding HTML editing features, changes to index, index2 is normal page, etc.

--- app.py
+++ app.py
@@ -5,24 +5,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 clients = 0
+message = ""
 
 @socketio.on('connect')
 def connect():
     global clients
     clients += 1
-    emit("users", {"user_count": clients}, broadcast = True)
+    emit("users", {"user_count": clients, "message" : message}, broadcast = True)
 
 @socketio.on('disconnect')
 def disconnect():
     global clients
     clients -=1
-    emit("users", {"user_count": clients}, broadcast= True)
-
+    emit("users", {"user_count": clients, "message" : message}, broadcast= True)
 
 
 @socketio.on('message')
 def handleMessage(msg):
     print('Message: ' + msg)
+    global message
+    message = msg
     send(msg, broadcast=True) #broadcasting received message to all connect clients, including the one that sent it.
 
 if __name__ == '__main__':
