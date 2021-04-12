bdec41132c322e957909bb391744711b48ca61f8
Frankie <32919119+frankiegold@users.noreply.github.com>
2021-04-11 22:35:34 -0400

    Added alternative DB connection mechanism

--- login.py
+++ login.py
@@ -1,24 +1,25 @@
 # Import statements
 from flask import Flask,render_template,request,redirect,url_for
 from flaskext.mysql import MySQL
-import MYSQLdb
 
-# mysql = MySQL()
-# Container for app
-app = Flask(_name_)
+mysql = MySQL()
+app = Flask(__name__)
 
-# Possible other way to connect
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'root_password'
-# app.config['MYSQL_DATABASE_DB'] = 'textAndShareDB'
-# app.config['MYSQL_DATABASE_HOST'] = '69.121.70.211:3306'
- 
-# Connection to MySQL database
-conn = MySQLdb.connect(host="69.121.70.211:3306",user="root",password="root_password",db="textAndShareDB") 
+# Connect to SQL database
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root_password'
+app.config['MYSQL_DATABASE_DB'] = 'textAndShareDB'
+app.config['MYSQL_DATABASE_HOST'] = '69.121.70.211:3306'
+mysql.init_app(app)
+
+con = mysql.connect()
+
+# Alternative connection to MySQL database (couldn't figure out how to properly import MySQLdb)
+# con = MySQLdb.connect(host="69.121.70.211:3306",user="root",password="root_password",db="textAndShareDB") 
 
 # First page
 @app.route("/")
-def index():
+def root():
 	return render_template("signup.html", title="signUp")
 
 # Sign up method
@@ -28,12 +29,10 @@ def signUp():
 	password = str(request.form["password"])
 	email = str(request.form["email"])
 	
-	# cursor = mysql.connect().cursor()
-	cursor = conn.cursor()
-	
+	cursor = mysql.connect().cursor()
 	cursor.execute("INSERT INTO users (name,password,email)VALUES(%s,%s,%s)",(username,password,email))
 
-	conn.commit()
+	con.commit()
 
 	return redirect(url_for("login"))
 
@@ -47,17 +46,19 @@ def login():
 def check():
 	username = str(request.form["user"])
 	password = str(request.form["password"])
-	cursor = conn.cursor()
+
+	cursor = mysql.connect().cursor()
 	cursor.execute("SELECT * FROM users WHERE username=' "+ username + "' AND password =' " + password + " ' ")
 
 	user = cursor.fetchone()
 	
+	# Authenticate conditional
 	if user is None:
 		return "Username or password is incorrect"
 	else:
 		return redirect(url_for("index"))
 
-# Takes you to main page
+# Takes you to main editor 
 @app.route("/index")
 def index():
 	return render_template("index.html")
